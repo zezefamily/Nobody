0c7037c6e9136a987ad889aeffc7924751274a92
泽泽 <zezefamily@163.com>
2020-12-01 19:03:40 +0800

    修改配置

--- nobody/src/App.vue
+++ nobody/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <HomePage msg="首页"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HomePage from './components/home/home.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    HelloWorld
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0px;
+  margin-top: 60px;
 }
 </style>
